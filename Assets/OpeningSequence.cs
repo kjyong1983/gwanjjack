3873984d35a98a97e86c0f51a5deb5ede7b5ac0f
MW Lee <ionizn@gmail.com>
2016-10-30 00:14:08 +0900

    튜토리얼 씬 추가

---
+++ Assets/OpeningSequence.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using PlatformerPro;
+public class OpeningSequence : MonoBehaviour {
+
+	public GameObject coffin_pre;
+	public GameObject coffin;
+	public GameObject player;
+	public GameObject kitty;
+	public GameObject arrow;
+	public GameObject arrow2;
+
+	public GameObject mummy;
+
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine("GameStart");
+		
+		//player.GetComponent<GroundMovement>()as.Speed(0);
+		iTween.MoveTo(kitty, iTween.Hash("path",iTweenPath.GetPath("kittypath"),"time",5f));
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	public IEnumerator GameStart()
+    {
+  		yield return new WaitForSeconds(0.1f);
+
+		coffin.SetActive(true);
+		coffin_pre.SetActive(false);
+  		yield return new WaitForSeconds(0.5f);
+		player.SetActive(true);
+		kitty.SetActive(false);	
+		//mummy.transform.localScale = new Vector3(1f,1f,;
+		mummy.SetActive(true);	
+
+  		yield return new WaitForSeconds(0.5f);
+
+		arrow.SetActive(true);	
+		arrow2.SetActive(true);	
+
+    }
+
+
+}
