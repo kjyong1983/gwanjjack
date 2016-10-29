c9415a30a684bd552b95b9be0c726cae8a48fb9f
MW Lee <ionizn@gmail.com>
2016-10-29 22:03:15 +0900

    적 행동 수정 ( 주인공 근접 시 활성화 하도록 )

---
+++ Assets/Stage/Scripts/EnemySpawner.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawner : MonoBehaviour 
+{
+
+	[SerializeField]
+     public float range = 5.0f;
+ 
+     private Transform t;
+     public Transform player;
+ 
+     private void Awake()
+     {
+         t = this.transform;
+		 InvokeRepeating( "CheckDistance", 1f, 1f);
+		 gameObject.SetActive(false);
+	 }
+ 
+     public void CheckDistance()
+     {
+         if(player)
+		 {
+			float  dist = Vector3.Distance(transform.position, player.position);
+			if(dist <= range)
+			{
+				Debug.Log("Dist : "+ dist);
+				 gameObject.SetActive(true);
+				CancelInvoke("CheckDistance");
+			}
+		 }
+	 }
+
+	private void OnDrawGizmos(){
+		UnityEditor.Handles.color = Color.yellow;
+		UnityEditor.Handles.DrawWireDisc(this.transform.position ,Vector3.back, range);
+	}
+
+
+
+}
